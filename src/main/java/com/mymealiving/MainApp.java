e9b7ed24b6c7aff07ff7abcb97a0ccd9626f1d6e
Scott Semtner <sjsemtner@gmail.com>
2020-03-12 19:03:00 -0700

    fixed fxml warning

--- src/main/java/com/mymealiving/MainApp.java
+++ src/main/java/com/mymealiving/MainApp.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+@SuppressWarnings("unchecked")
 public class MainApp extends Application {
 
     public static String toDisplayCase(String s) {
